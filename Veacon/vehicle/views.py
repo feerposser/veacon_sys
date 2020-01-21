abb5477be98fdaa6486b0af4c9ace84c9e75f696
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2020-01-21 15:03:46 -0300

    atualização do módulo vehicle

--- Veacon/vehicle/views.py
+++ Veacon/vehicle/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from .manage_data import get_vehicles
 
 
+@login_required(login_url='/login/')
 def vehicles(request):
     vehicles_list = get_vehicles(request.user)
     return render(request, 'vehicles.html', {'vehicles': vehicles_list})
